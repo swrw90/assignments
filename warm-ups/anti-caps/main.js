cf82d0944ef06d740c0443684a50ebf9a5d6f2ee
Seth Watson <seth.watson1@gmail.com>
2017-06-28 23:01:12 -0600

    Clean up some files and added a few new notes

--- warm-ups/anti-caps/main.js
+++ warm-ups/anti-caps/main.js
@@ -1,15 +1,4 @@
-//function capitalize(string) {
-//    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-//}
-//
-//
-//function antiCaps('Hello')  
-//
-//function isCaps(letter) {  
-//  return letter === letter.toUpperCase();
-//}
-
 function antiCaps(text) {
     return text.charAt(1).toUpperCase() + text.slice().toLowerCase(2);
 }
-console.log(antiCaps("THIS IS A STRING"));
+console.log(antiCaps("ThIs Is A sTrInG"));
